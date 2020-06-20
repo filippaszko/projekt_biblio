1d858f3a94395a09a34aac19da3faaccbe3e7aed
DamianM95-git <61930899+DamianM95-git@users.noreply.github.com>
2020-06-20 19:39:45 +0200

    Update Ksiazka.java

--- library/class/Ksiazka.java
+++ library/class/Ksiazka.java
@@ -1,6 +1,8 @@
 package library.class;
 
-public class Ksiazka extends Autor, interface Genre {
+public class Ksiazka extends Autor{              // wydaje mi się że za "Autor" powinna być {, a później } po Genre, bez tego nie pójdzie
+interface Genre {
+}
 	  private int id;
 	  private String tytul;
 	  private int liczbaStron;
@@ -17,9 +19,23 @@ public class Ksiazka extends Autor, interface Genre {
 	    this.tytul = tytul;
 	  }
 	
+	String podajtytul() {                // metoda wywołująca dodanie nowego tytułu ksiazki (do sprawdzenia / poprawienia)
+		  Scanner in = new Scanner(System.in);
+		  System.out.println("Podaj tytuł nowej ksiązki, którą chcesz dodać");
+		  String tytul = in.nextLine();
+		  return tytul;
+	  }
+	
 	  public void setLStron(int liczbastr) {
 	     this.liczbastr = liczbaStron;
 	  }
+	int podajLstron() {             // metoda wywołująca dodanie liczby stron do ksiązki (do sprawdzenia / poprawienia)
+		Scanner in = new Scanner(System.in);
+		System.out.println("Podaj liczbę stron nowej ksiązki, którą dodałeś");
+		String lStron = in.nextLine();
+		int liczbaStron = Integer.parseInt(lStron);
+		return liczbaStron;
+	 }
 	
 	  public void setAutor(Autor autor) {
 	    this.autor = autor;
