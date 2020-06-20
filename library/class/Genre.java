ecf568bbbb5d63f8e6330372965229cf273ca8e1
filippaszko <61930907+filippaszko@users.noreply.github.com>
2020-06-20 10:25:46 +0200

    Create Genre.java

---
+++ library/class/Genre.java
@@ -0,0 +1,13 @@
+package library.class;
+
+public class Genre {
+	  private String gatunek;
+
+	  public String getGatunek() {
+	    return gatunek;
+	  }
+
+	  public void setGatunek(String gatunek) {
+	    this.gatunek = gatunek;
+    }
+}
