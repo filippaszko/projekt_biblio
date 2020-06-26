b6fd9268af3bab9f20440bcee32c67ddb77a1e25
DamianM95-git <61930899+DamianM95-git@users.noreply.github.com>
2020-06-27 01:00:42 +0200

    Update Ksiazka.java

--- library/class/Ksiazka.java
+++ library/class/Ksiazka.java
@@ -18,17 +18,19 @@ interface Genre {
 	  public void setTytul(String tytul) {
 	    this.tytul = tytul;
 	  }
-	
+	/*
 	String podajtytul() {                // metoda wywołująca dodanie nowego tytułu ksiazki (do sprawdzenia / poprawienia)
 		  Scanner in = new Scanner(System.in);
 		  System.out.println("Podaj tytuł nowej ksiązki, którą chcesz dodać");
 		  String tytul = in.nextLine();
 		  return tytul;
 	  }
+	  */
 	
 	  public void setLStron(int liczbastr) {
 	     this.liczbastr = liczbaStron;
 	  }
+	/*
 	int podajLstron() {             // metoda wywołująca dodanie liczby stron do ksiązki (do sprawdzenia / poprawienia)
 		Scanner in = new Scanner(System.in);
 		System.out.println("Podaj liczbę stron nowej ksiązki, którą dodałeś");
@@ -36,6 +38,7 @@ interface Genre {
 		int liczbaStron = Integer.parseInt(lStron);
 		return liczbaStron;
 	 }
+	 */
 	
 	  public void setAutor(Autor autor) {
 	    this.autor = autor;
