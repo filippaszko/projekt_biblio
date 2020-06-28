010bd69ab47bc029a8a35d8cdde0fd83a01d7cf0
DamianM95-git <61930899+DamianM95-git@users.noreply.github.com>
2020-06-28 18:03:48 +0200

    Update Autor.java

--- library/class/Autor.java
+++ library/class/Autor.java
@@ -1,26 +1,26 @@
 package library.class;
 
+/**
+W klasie celowo nie zastosowano metod typu get i set. Nie widzielismy potrzeby generowania martwych metod w celu podania/zwrócenia poszczególnych elementów danego obiektu. Nie było planów wykorzystywania tego rodzaju metod. W celu maksymalnego uproszczenia kodu nie zostały one zaimplementowane.
+**/
 public class Autor {
-	  private String imie;
-	  private String nazwisko;
+	  String imie;
+	  String nazwisko;
 
-	  public String getImie() {
-	    return imie;
+  //konstruktory
+  public Autor(){
+    imie = "Paulo";
+    nazwisko = "Coelho";
+  }
+
+  public Autor(String imie, String nazwisko){
+    this.imie = imie;
+    this.nazwisko = nazwisko;
+  }
+
+  //metoda
+  public String toString(){
+	  return imie + " " + nazwisko;
 	  }
 
-	  public void setImie(String imie) {
-	    this.imie = imie;
-	  }
-
-	  public String getNazwisko() {
-	    return nazwisko;
-	  }
-
-	  public void setNazwisko(String nazwisko) {
-	    this.nazwisko = nazwisko;
-	  }
-
-	  @Override public String toString() {
-	    return imie + " " + nazwisko;
-	  }
-	}
+}
